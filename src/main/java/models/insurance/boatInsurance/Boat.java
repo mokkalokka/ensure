f85345420834c068256d5d6cab8a86cad91e590e
mokkalokka <michael.larsen90@gmail.com>
2019-03-28 11:39:42 +0100

    Lagt til JFoenix

--- src/main/java/models/insurance/boatInsurance/Boat.java
+++ src/main/java/models/insurance/boatInsurance/Boat.java
@@ -6,17 +6,17 @@ public class Boat {
     private String registrationNr;
     private String boatType;
     private String boatModel;
-    private BoatOwner owner;
+    //private BoatOwner owner;
     private double lengthInft;
     private Year modelYear;
     private String engineType;
     private int engineHP;
 
-    public Boat(String registrationNr, String boatType, String boatModel, BoatOwner owner, double lengthInfeet, Year modelYear, String engineType, int engineHP) {
+    public Boat(String registrationNr, String boatType, String boatModel, /*BoatOwnerowner,*/  double lengthInfeet, Year modelYear, String engineType, int engineHP) {
         this.registrationNr = registrationNr;
         this.boatType = boatType;
         this.boatModel = boatModel;
-        this.owner = owner;
+        //this.owner = owner;
         this.lengthInft = lengthInft;
         this.modelYear = modelYear;
         this.engineType = engineType;
