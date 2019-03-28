2a744329b47fbb761a42b0660689a92a7cd2fc82
eskilgaarehostad <eskilgh@gmail.com>
2019-03-28 11:24:59 +0100

    La til invoiceAdress i konstruktør for Customer. Fikset skrivefeil i address.

--- src/main/java/models/customer/Customer.java
+++ src/main/java/models/customer/Customer.java
@@ -3,24 +3,27 @@ package models.customer;
 import models.insurance.AccidentStatement;
 import models.insurance.Insurance;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Customer {
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 5853437654367638991L;
     private static final AtomicInteger NEXT_INSURANCE_NR = new AtomicInteger(10000); // TODO: finn en robust måte å hente indeks på, fiks dette i AccidentStatement.accidentNr også.
     private String firstName;
     private String lastName;
     private Date customerSince;
-    private String invoiceAdress;
+    private String invoiceAddress;
     private int insuranceNr;
     private ArrayList<Insurance> listOfInsurances;
     private ArrayList<AccidentStatement> accidentStatements; // skademelding
     private String pendingCompensation; // TODO: finn en måte å strukturere data på.
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, String invoiceAddress) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.invoiceAddress = invoiceAddress;
         this.customerSince = new Date();
         this.insuranceNr = NEXT_INSURANCE_NR.getAndIncrement();
     }
