c708e036fe5e11ac68668cfdb0838bb1f8f3ca05
mokkalokka <michael.larsen90@gmail.com>
2019-03-29 16:13:34 +0100

    * Lagt til en test for customerHandling * Mulighet til å navigere tilbake

--- src/main/java/controllers/newCustomerController.java
+++ src/main/java/controllers/newCustomerController.java
@@ -63,7 +63,6 @@ public class newCustomerController {
         }
     }
 
-
     @FXML
     private void updateStatus(String message){
         int customerCount = ListOfCustomers.getCustomerCount();
