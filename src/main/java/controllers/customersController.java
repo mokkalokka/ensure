33858d3a3723bde1cbb7f9052536ec027deed3d7
Jonathan Kjølstad <jonathan.kjolstad@gmail.com>
2019-04-02 22:19:28 +0200

    Kommentert kode grundig

--- src/main/java/controllers/customersController.java
+++ src/main/java/controllers/customersController.java
@@ -56,17 +56,20 @@ public class customersController {
         openScene.openScene(event, "/org/view/newCustomer.fxml");
     }
 
-    
+
     public void doubleClicked(Customer clickedCustomer) {
         //TODO maa erstatte vindu, ikke aapne nytt
+        //TODO feilhandering
         try {
+            //Last inn ny fxml
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/view/detailedCustomer.fxml"));
             Parent root = loader.load();
 
+            //Finner kontrolleren til fxml fila og passerer den dobbelklikkede kunden til kontrolleren
             detailedCustomerController controller = loader.getController();
-
             controller.pickCustomer(clickedCustomer);
 
+            //Visning av nye vindu
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
             stage.show();
@@ -80,11 +83,17 @@ public class customersController {
     public void initialize() {
         //Klikking paa kunder
 
+        //Funksjon pa alle rader in tablet
         tblCustomer.setRowFactory(tableView -> {
-            TableRow<Customer> aRow = new TableRow<>();
-            aRow.setOnMouseClicked(mouseEvent -> {
-                if ((! aRow.isEmpty() && mouseEvent.getClickCount() == 2)) {
 
+            //Tom rad
+            TableRow<Customer> aRow = new TableRow<>();
+
+            //rad far listener paa museklikk
+            aRow.setOnMouseClicked(mouseEvent -> {
+                //Hivs raden har innhold og klikket var et dobbeltklikk
+                if ((! aRow.isEmpty() && mouseEvent.getClickCount() == 2)) {
+                    //Kall pa dobleCliked med Customerobkjetet til raden
                     doubleClicked(aRow.getItem());
                 }
             });
@@ -105,23 +114,38 @@ public class customersController {
         //Alle kunder til en filteret liste uten filter
         FilteredList<Customer> filteredData = new FilteredList<>(observableCustomerList, null);
 
+        //Listener paa soekeboksen
         txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            //Hver gang innholden i sokeboksen endres kalles denne
+            //Funskjon pa alle elementene i filterData som forventer en boolks verdi.
+            //True: elementet forblir i lista
+            //False: elmentet blir fjernet
             filteredData.setPredicate(aCustomer -> {
+
+                //Hvis soekeboksen er tom, returnerer false
                 if (newValue == null || newValue.isEmpty()) {
                     return true;
                 }
 
+                //Hvis sokedata i et element innholder det i sokeboksen, returnerer true
                 String lowerCaseFilter = newValue.toLowerCase();
 
                 if (aCustomer.searchData().contains(lowerCaseFilter)) {
                     return true;
                 }
+                //Hvis sokedata ikke stemmer og boksen ikke er tom, returnerer false
                 return false;
             });
         });
 
+        //Putter ale elementete fra den na filtrerte lista i en javafx sortedlist
         SortedList<Customer> sortedData = new SortedList<>(filteredData);
+
+        //Binder lista til javafx comparetorproperty, denne gir muligheten til sortering av tablet automatisk.
         sortedData.comparatorProperty().bind(tblCustomer.comparatorProperty());
+
+        //Oppdaterer tablet
         tblCustomer.setItems(sortedData);
 
     }
