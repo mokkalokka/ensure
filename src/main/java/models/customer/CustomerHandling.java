6937bafd0f2bd1910a1c17d8713f6ab094c44c83
mokkalokka <michael.larsen90@gmail.com>
2019-04-02 21:52:19 +0200

    CustomerHandling.createNewCustomer returnerer nå en boolean

--- src/main/java/models/customer/CustomerHandling.java
+++ src/main/java/models/customer/CustomerHandling.java
@@ -2,32 +2,22 @@ package models.customer;
 
 public class CustomerHandling {
 
-
-    // Tror denne metode heller burde returnere true/false slik at man kan handle alt som skal vises i controller.
-    public String createNewCustomer(String firstName, String lastName, String invoiceAddress){
+    public boolean createNewCustomer(String firstName, String lastName, String invoiceAddress){
         if(!stringContainsNumbers(firstName) && !stringContainsNumbers(lastName)){
-        Customer customer = new Customer(firstName,lastName,invoiceAddress);
-        addToCustomersList(customer);
-        return "Kunden er lagt til i listen";
+            Customer customer = new Customer(firstName,lastName,invoiceAddress);
+            ListOfCustomers.addCustomer(customer);
+            return true;
         }
-
         else{
             //TODO:Throw new exception (Lag denne)
             //TODO:Håndtere invoice Address
-            return "Feil: navn og etternavn kan ikke \ninneholde noen siffer";
+            return false;
         }
     }
-
-    //Legger til kunden i ListOfCustomers
-    private void addToCustomersList(Customer customer) {
-        ListOfCustomers.addCustomer(customer);
-    }
-
 
     //Sjekker om en String inneholder nummer
     public boolean stringContainsNumbers(String string){
         return string.matches(".*\\d.*");
     }
-
-
 }
+
