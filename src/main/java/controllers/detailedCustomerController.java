fee7363542db7200debd23f7bddb612201dd53f4
Jonathan Kjølstad <jonathan.kjolstad@gmail.com>
2019-04-11 17:24:40 +0200

    Tablevisning av skademeldinger og forskiringer Listener på alle kollonner i begge tabellene

--- src/main/java/controllers/detailedCustomerController.java
+++ src/main/java/controllers/detailedCustomerController.java
@@ -1,24 +1,72 @@
 package controllers;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import models.customer.Customer;
 import models.gui.WindowHandler;
+import models.insurance.AccidentStatement;
 import models.insurance.Insurance;
 import models.insurance.boatInsurance.BoatInsurance;
 import models.insurance.residenceInsurance.PrimaryResidenceInsurance;
 import models.travelInsurance.TravelInsurance;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class detailedCustomerController {
 
     private Customer currentCustomer;
+    ObservableList<Insurance> insuranceObservableList;
+    ObservableList<AccidentStatement> accidentStatementsObservableList;
+
+
+    //Insurance table
+    @FXML
+    private TableView<Insurance> tblInsurance;
+
+    @FXML
+    private TableColumn<Insurance, String> clmnInsuranceType;
+
+    @FXML
+    private TableColumn<Insurance, LocalDate> clmnJoinDate;
+
+    @FXML
+    private TableColumn<Insurance, Double> clmnTotal;
+
+    @FXML
+    private TableColumn<Insurance, Double> clmnAnnualPremium;
+
+    @FXML
+    private TableColumn<Insurance, String> clmnCoverageDescription;
+
+    //AccidentStatements table
+    @FXML
+    private TableView<AccidentStatement> tblAccidentStatement;
+
+    @FXML
+    private TableColumn<AccidentStatement, Integer> clmnAccidentNr;
+
+    @FXML
+    private TableColumn<AccidentStatement, LocalDate> clmnDateOfAccident;
+
+    @FXML
+    private TableColumn<AccidentStatement, String> clmnAccidentType;
+
+    @FXML
+    private TableColumn<AccidentStatement, String> clmnAccidentDescription;
 
     //FX elementene
     @FXML
@@ -86,6 +134,10 @@ public class detailedCustomerController {
         openInsuranceWindow(clickedInsurance);
     }
 
+    private void accidentStatementDblClicked(AccidentStatement clickedAccidentStatement) {
+
+    }
+
     //Finner nåværende stage ved hjelp av en fx:id for å kunne lukke dette vinduet
     private Stage getCurrentStage(){
         return (Stage) lblSurname.getScene().getWindow();
@@ -93,8 +145,14 @@ public class detailedCustomerController {
 
 
     public void pickCustomer(Customer aCustomer) {
-        //Lokal variabel for kunden som vises
+        //Lokal variabel for kunden som vises, og arrayene til kunden til observablelister
         currentCustomer = aCustomer;
+        insuranceObservableList = FXCollections.observableArrayList(currentCustomer.getListOfInsurances());
+        accidentStatementsObservableList = FXCollections.observableList(currentCustomer.getListOfAccidentStatements());
+
+        //Hjelp metoder siden dette ikke kan ligge i den innebygde initialize metoden
+        initializeInsuranceTable();
+        initializeAccidentStatementTable();
 
         //Setter textboksene
         lblInsuranceNr.setText(String.valueOf(aCustomer.getInsuranceNr()));
@@ -102,6 +160,75 @@ public class detailedCustomerController {
         lblFirstName.setText(aCustomer.getFirstName());
         lblCustomerSince.setText(aCustomer.getCustomerSince().toString());
         lblInvoiceAddress.setText(aCustomer.getInvoiceAddress());
+    }
+    
+
+    private void initializeInsuranceTable() {
+        //Valuefactory paa alle kollonner som bruker get metodene til customer
+        clmnInsuranceType.setCellValueFactory(new PropertyValueFactory<>("insurnanceName"));
+        clmnJoinDate.setCellValueFactory(new PropertyValueFactory<>("dateOfIssue"));
+        clmnTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+        clmnAnnualPremium.setCellValueFactory(new PropertyValueFactory<>("annualPremium"));
+        clmnCoverageDescription.setCellValueFactory(new PropertyValueFactory<>("coverageDescription"));
+
+
+        SortedList<Insurance> sortedData = new SortedList<>(insuranceObservableList);
+
+        //Binder lista til javafx comparetorproperty, denne gir muligheten til sortering av tablet automatisk.
+        sortedData.comparatorProperty().bind(tblInsurance.comparatorProperty());
+
+        //Oppdaterer tablet
+        tblInsurance.setItems(sortedData);
+
+
+        tblInsurance.setRowFactory(tableView -> {
+
+            //Tom rad
+            TableRow<Insurance> aRow = new TableRow<>();
+
+            //rad far listener paa museklikk
+            aRow.setOnMouseClicked(mouseEvent -> {
+                //Hivs raden har innhold og klikket var et dobbeltklikk
+                if ((! aRow.isEmpty() && mouseEvent.getClickCount() == 2)) {
+                    //Kall pa dobleCliked med Customerobkjetet til raden
+                    insuranceDblClicked(aRow.getItem());
+                }
+            });
+            return aRow;
+        });
+    }
+
+    private void initializeAccidentStatementTable() {
+        //Valuefactory paa alle kollonner som bruker get metodene til customer
+        clmnAccidentNr.setCellValueFactory(new PropertyValueFactory<>("accidentNr"));
+        clmnDateOfAccident.setCellValueFactory(new PropertyValueFactory<>("dateOfAccident"));
+        clmnAccidentType.setCellValueFactory(new PropertyValueFactory<>("accidentType"));
+        clmnAccidentDescription.setCellValueFactory(new PropertyValueFactory<>("accidentDescription"));
+
+
+        SortedList<AccidentStatement> sortedData = new SortedList<>(accidentStatementsObservableList);
+
+        //Binder lista til javafx comparetorproperty, denne gir muligheten til sortering av tablet automatisk.
+        sortedData.comparatorProperty().bind(tblAccidentStatement.comparatorProperty());
+
+        //Oppdaterer tablet
+        tblAccidentStatement.setItems(sortedData);
+
+        tblAccidentStatement.setRowFactory(tableView -> {
+
+            //Tom rad
+            TableRow<AccidentStatement> aRow = new TableRow<>();
+
+            //rad far listener paa museklikk
+            aRow.setOnMouseClicked(mouseEvent -> {
+                //Hivs raden har innhold og klikket var et dobbeltklikk
+                if ((! aRow.isEmpty() && mouseEvent.getClickCount() == 2)) {
+                    //Kall pa dobleCliked med Customerobkjetet til raden
+                    accidentStatementDblClicked(aRow.getItem());
+                }
+            });
+            return aRow;
+        });
     }
 
     @FXML
