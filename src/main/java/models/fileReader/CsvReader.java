8644cc1e314e40bbe06a835003207d27a024275c
mokkalokka <michael.larsen90@gmail.com>
2019-04-12 12:55:51 +0200

    Lagt til parsing av accidentStatement (mangler å legge til i kunde)

--- src/main/java/models/fileReader/CsvReader.java
+++ src/main/java/models/fileReader/CsvReader.java
@@ -81,6 +81,14 @@ public class CsvReader {
     private void parseAccidentStatement(String[] lineArray) {
         AccidentStatement accidentStatement = new AccidentStatementBuilder()
                 .setRegisteredTo(lineArray[0])
+                .setAccidentNr(lineArray[1])
+                .setDateOfAccident(lineArray[2])
+                .setAppraisalAmount(lineArray[3])
+                .setAccidentDescription(lineArray[4])
+                .setAccidentType(lineArray[5])
+                .setDispersedCompensation(lineArray[6])
+                .build();
+
 
 
 /*
