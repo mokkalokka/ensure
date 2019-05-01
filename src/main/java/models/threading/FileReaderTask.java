1a48720211e63f349a0d3ba0950c7a56d0aca714
mokkalokka <michael.larsen90@gmail.com>
2019-05-01 17:16:53 +0200

    Fjernet thread sleep

--- src/main/java/models/threading/FileReaderTask.java
+++ src/main/java/models/threading/FileReaderTask.java
@@ -16,7 +16,6 @@ public class FileReaderTask extends Task{
 
     @Override
     protected Object call() throws Exception {
-        Thread.sleep(10_000);
 
         if (fileExtension.equals("csv")) {
             CsvReader csvReader = new CsvReader(path);
